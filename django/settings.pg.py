96f1cac05adfc286307b3c1889368fd2ae96e07b
imyme6yo <imyme6yo@gmail.com>
2020-07-02 23:24:32 +0900

    Modify: django settings.pg.py

--- django/settings.pg.py
+++ django/settings.pg.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-extensions',
+    'django_extensions',
     'django_filters',
     'rest_framework',
     'rest_framework_swagger',
