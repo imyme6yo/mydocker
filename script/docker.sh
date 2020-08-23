4ead7378e7bd5f630b2c0cb6959b68ba7561dddf
imyme6yo <imyme6yo@gmail.com>
2020-08-24 01:49:04 +0900

    Modify: @python, script

--- script/docker.sh
+++ script/docker.sh
@@ -2,18 +2,17 @@
 # @AUTHOR: imyme6yo "imyme6yo@gmail.com"
 # @DRAFT: 20200320
 # @UPDATE: 20200405
-MODE="project"
-DIR="code"
+MODE="dev"
 PROJECT="myapp"
 
 # Set MODE
 if [ -n "$1" ] ; then
-    if ! [ "$1" = "project" -o "$1" = "start" ] 
-    then
-        echo "Mode is `$1`\\n Mode should be either start or project."
-        exit -1
-    fi
-    MODE=$1
+  if ! [ "$1" = "dev" -o "$1" = "start" ] 
+  then
+    echo "Mode is `$1`\\n Mode should be either start or project."
+    exit -1
+  fi
+  MODE=$1
 fi
 
 # Set NAME
@@ -22,7 +21,7 @@ if [ -n "$2" ] ; then
 fi
 
 # Set container name and image tag.
-CONTAINER="$PROJECT-01"
+CONTAINER="$PROJECT"
 IMAGE="$PROJECT:dev"
 
 # stop docker container
@@ -31,14 +30,22 @@ docker stop $CONTAINER
 docker rm $CONTAINER
 # remove docker image
 docker rmi $IMAGE
-if [ "$MODE" = "project" ]; then
-    # build image
-    docker build --force-rm --build-arg DIR=$DIR --build-arg PROJECT=$PROJECT -t $IMAGE -f ./Dockerfile.project .
-    # run container
-    docker run --rm -it -v $(pwd):/code --name $CONTAINER $IMAGE sh /code/project.sh
+if [ "$MODE" = "dev" ]; then
+  # build image
+  docker build --force-rm \
+    --build-arg PROJECT=$PROJECT \
+    -t $IMAGE -f ./Dockerfile.dev .
+  # run container
+  docker run --rm -it \
+    -v $(pwd):/$PROJECT \
+    --name $CONTAINER $IMAGE sh /$PROJECT/dev.sh
 elif [ "$MODE" = "start" ]; then
-    # build image
-    docker build --force-rm --build-arg DIR=$DIR --build-arg PROJECT=$PROJECT -t $IMAGE -f ./Dockerfile.startup .
-    # run container
-    docker run --rm -it -v $(pwd):/code --name $CONTAINER $IMAGE sh /code/startup.sh
+  # build image
+  docker build --force-rm \
+    --build-arg PROJECT=$PROJECT \
+    -t $IMAGE -f ./Dockerfile.start .
+  # run container
+  docker run --rm -it \
+    -v $(pwd):/$PROJECT \
+    --name $CONTAINER $IMAGE sh /$PROJECT/startup.sh
 fi
--- script/startup.sh
+++ script/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # @AUTHOR: imyme6yo "imyme6yo@gmail.com"
 # @DRAFT: 20200320
-touch requirements.txt
-mkdir ${PROJECT}
+# @UPDATE: 20200824
+pip freeze > ${REQUIREMENT}
