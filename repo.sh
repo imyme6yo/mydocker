ee37316a6bc28e2966ab38c0dc50629c8204fc0f
imyme6yo <imyme6yo@gmail.com>
2021-04-10 10:28:14 +0900

    Add: add script for run docker repository

---
+++ repo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# @AUTHOR: imyme6yo "imyme6yo@gmail.com"
+# @DRAFT: 20210410
+DOCKER_REPO_PORT="$1"
+
+if [[ -z $DOCKER_REPO_PORT ]] ; then
+    DOCKER_REPO_PORT=7788
+fi
+
+docker run -dit --name docker-registry -p ${DOCKER_REPO_PORT}:${DOCKER_REPO_PORT} registry
