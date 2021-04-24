7fb6293a87cc5e869fc2e51774f7523057cc79e5
imyme6yo <imyme6yo@gmail.com>
2021-04-25 04:45:00 +0900

    @fastapi-postgres

--- migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# @AUTHOR: imyme6yo "imyme6yo@gmail.com"
-# @DRAFT: 20210425
-
-# alembic revision -m "my content" 
-alembic revision -m "my content" --autogenerate
-alembic upgrade head
