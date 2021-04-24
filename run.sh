6de86a1eb7be83517a446aa06a2a773cee2d6474
imyme6yo <imyme6yo@gmail.com>
2021-04-24 23:22:37 +0900

    @fastapi

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # @AUTHOR: imyme6yo "imyme6yo@gmail.com"
 # @DRAFT: 20210424
-uvicorn main:app --reload --port ${SERVICE_CONTAINER_PORT}
-# uvicorn ${SERVICE}.main:app --reload --port ${SERVICE_CONTAINER_PORT}
+# uvicorn myapp.main:app --reload --port ${SERVICE_CONTAINER_PORT}
+uvicorn ${SERVICE}.main:app --reload --port ${SERVICE_CONTAINER_PORT}
